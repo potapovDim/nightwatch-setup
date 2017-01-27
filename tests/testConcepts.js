35e6e07799d0571a6b79bcd893be6be67b9e1853
potapovDim <dmytrop@weblium.com>
2017-01-27 16:56:40 +0200

    ()=> to function

--- tests/testConcepts.js
+++ tests/testConcepts.js
@@ -2,13 +2,17 @@
  function executedFunction () {
      console.log(window ,document)
  }
+
  export default {
-     beforeEach: (browser) => {
+     instance: null,
+     beforeEach: function(browser) {
+        this.instance = browser;
+        console.log(this)
         browser.url('http://localhost:8080/')
             .execute(disableTransition,[], () => console.log('remove styles'));
      },
-     'concepts tests with out login user fin group ': (browser) =>{
-         browser.resizeWindow(600,600)
+     'concepts tests with out login user fin group ': function(browser) {
+         this.instance.resizeWindow(600,600)
          .resizeWindow(600,1200)
      }
  }
