f94df2607be03291bc7c67534ecb7709e1095393
potapovDim <dmytrop@weblium.com>
2017-01-27 17:06:54 +0200

    refactor

--- tests/testConcepts.js
+++ tests/testConcepts.js
@@ -1,18 +1,17 @@
  const disableTransition  = require('../commands/offcssanimation.js').disableTransition
+
  function executedFunction () {
      console.log(window ,document)
  }
-
- export default {
+ module.exports =  {
      instance: null,
      beforeEach: function(browser) {
         this.instance = browser;
-        console.log(this)
         browser.url('http://localhost:8080/')
             .execute(disableTransition,[], () => console.log('remove styles'));
      },
      'concepts tests with out login user fin group ': function(browser) {
          this.instance.resizeWindow(600,600)
-         .resizeWindow(600,1200)
+         this.instance.resizeWindow(600,1200)
      }
  }
