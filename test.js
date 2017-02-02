b8fed2f6aa9b8aae85c5259d9e4dd420b6e1d83b
potapovDim <dmytrop@weblium.com>
2017-02-02 18:32:00 +0200

    data

--- test.js
+++ test.js
@@ -1,15 +1,25 @@
-const data = {
+const dataGet = {
   host: 'localhost',
   port: '3000',
   path: '/'
 }
+const dataPost = {
+  url: 'http://localhost:3000/',
+  method: 'POST',
+  json: true,
+  body: {
+    username: 'test@test.com',
+    password: 'testpasss'
+  }
+}
 
 const util = require('./utils')
-const http = require('http')
 const Rx = require('rxjs')
+const http = require('http')
+const request = require('request')
 
-const asyncGetData = (data) => new Promise((resolve, reject) => {
-  ( getData = (data) => {
+const asyncGetData = data => new Promise((resolve, reject) => {
+  (getData = data => {
     return http.get(data, response => {
       let body = ''
       response.on('data', d => {
@@ -23,9 +33,25 @@ const asyncGetData = (data) => new Promise((resolve, reject) => {
   })(data)
 })
 
-asyncGetData(data).then(data => console.log(data))
+const asyncPostData = data => new Promise((resolve, reject) => {
+  (postData = data => {
+    return request.post(data,(error, response, body) => {
+      console.log(response.statusCode, body)
+      if(!error && response.statusCode === 200) {
+        resolve(body)
+      }
+      else {
+        reject(error)
+      }
+    })
+  })(data)
+})
 
-Rx.Observable.from('hello world')
+
+asyncGetData(dataGet).then(data => console.log(data)).catch(e => console.log(e))
+asyncPostData(dataPost).then(data => console.log(data)).catch(e => console.log(e))
+
+Rx.Observable.of('hello world')
   .map(letter => letter.toUpperCase())
   .subscribe(
     x => console.log(x));
