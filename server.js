bd3952c859d966e1b2f4a63251d3a64db89a20d2
potapovDim <dmytrop@weblium.com>
2017-02-03 15:44:58 +0200

    refactor

--- server.js
+++ server.js
@@ -2,18 +2,39 @@ const Koa = require('koa')
 const app = new Koa()
 const router = require('koa-router')()
 const logger = require('koa-logger')
+const mongoose = require('mongoose')
 
-const PORT = 4422
+mongoose.connect('mongodb://127.0.0.1:27050')
+
+const testShema = mongoose.Schema({
+  name: String
+})
+
+const Testmodels = mongoose.model('testModel', testShema)
+
+const tt = new Testmodels({
+  name: 'test'
+})
+tt.save((err) => {
+  if(err) console.error(err.message)
+  else console.log('Save sucess')
+})
 
 const midd = (ctx) => new Promise((resolve,reject) => {
-  ctx.body = '!_@_!_@!_@!_'
-  resolve()
+  Testmodels.findOne({}, (err, users) => {
+    if(err) console.error(err.message, err.code);
+     resolve(users)
+      return ctx.body = users
+  }).then(users => {console.log(users)
+    ctx.body = users})
 })
+
 router.get('', midd)
 
 app.use(logger())
 app.use(router.routes())
 
+const PORT = 4422
 
 app.listen(PORT)
 console.log('server start on port ' + PORT)
