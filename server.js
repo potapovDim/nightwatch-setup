e3f55cd912dabeccbcfe60942fa10259c2bf41c8
potapovDim <dmytrop@weblium.com>
2017-02-03 16:36:46 +0200

    refactor

--- server.js
+++ server.js
@@ -15,21 +15,28 @@ const Testmodels = mongoose.model('testModel', testShema)
 const tt = new Testmodels({
   name: 'test'
 })
-tt.save((err) => {
-  if(err) console.error(err.message)
-  else console.log('Save sucess')
-})
 
-const midd = (ctx) => new Promise((resolve,reject) => {
+const getData = ctx => new Promise((resolve, reject) => {
   Testmodels.findOne({}, (err, users) => {
     if(err) console.error(err.message, err.code);
      resolve(users)
       return ctx.body = users
-  }).then(users => {console.log(users)
-    ctx.body = users})
+  }).then(users => ctx.body = users)
 })
 
-router.get('', midd)
+const postData = ctx => new Promise((resolve, reject) => {
+  console.log(ctx.body, '!!!!!!!!!!!')
+  tt.save((err) => {
+    if(err) reject(err);
+    else {
+      resolve('test data sucess save')
+      return ctx.body = 'test data sucess save'
+    }
+  })
+})
+
+router.get('', getData)
+router.post('', postData)
 
 app.use(logger())
 app.use(router.routes())
--- test.js
+++ test.js
@@ -1,18 +1,33 @@
 const {expect} = require('chai')
 const dataGet = {
   host: 'localhost',
-  port: '3000',
+  port: '4422',
   path: '/'
 }
-const dataPost = {
-  url: 'http://localhost:3000/',
+// const dataPost = {
+//   host: 'localhost',
+//   port: '4422',
+//   method: 'POST',
+//   json: true,
+//   body: 
+// }
+
+
+var options = {
+  hostname: 'localhost',
+  port: 4422,
+  path: '/',
   method: 'POST',
-  json: true,
-  body: {
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(JSON.stringify({
     username: 'test@test.com',
     password: 'testpasss'
+  }))
   }
 }
+
+
 
 const util = require('./utils')
 const Rx = require('rxjs')
@@ -20,12 +35,13 @@ const http = require('http')
 const request = require('request')
 
 const asyncGetData = data => new Promise((resolve, reject) => {
+  console.log("!!!!!!!!!!!");
   (getData = data => {
     return http.get(data, response => {
       let body = ''
       response.on('data', response => {
         body += response.toString('utf8')
-        if(body.statusCode ===404){
+        if (body.statusCode === 404) {
           reject(body)
         }
       });
@@ -36,32 +52,35 @@ const asyncGetData = data => new Promise((resolve, reject) => {
   })(data)
 })
 
-const asyncPostData = data => new Promise((resolve, reject) => {
-  (postData = data => {
-    return request.post(data,(error, response, body) => {
-      console.log(response.statusCode, body)
-      if(!error && response.statusCode === 200) {
-        resolve(body)
-      }
-      else {
-        reject(error)
-      }
+const asyncPostData = option => new Promise((resolve, reject) => {
+  console.log('-------------------', option);
+  (postData = option => {
+    return http.request(option, (response) => {
+      console.log("insidedsalkdlasjdlkasjlkfasjfjsafhajshfasfjsafjsajhfhajshlfjsahlfjs")
+      res.on('data', (chunk) => {
+        console.log(`BODY: ${chunk}`);
+      });
+      res.on('end', () => {
+        console.log('No more data in response.');
+      });
     })
-  })(data)
+  })(option).write(option).end()
 })
 
-
-//asyncGetData(dataGet).then(data => console.log(data)).catch(e => console.log(e))
-//asyncPostData(dataPost).then(data => console.log(data)).catch(e => console.log(e))
-
-// Rx.Observable.of('hello world')
-//   .map(letter => letter.toUpperCase())
-//   .subscribe(
-//     x => console.log(x));
+// asyncGetData(dataGet).then(data => console.log(data)).catch(e =>
+// console.log(e)) asyncPostData(dataPost).then(data =>
+// console.log(data)).catch(e => console.log(e)) Rx.Observable.of('hello world')
+//   .map(letter => letter.toUpperCase())   .subscribe(     x =>
+// console.log(x));
 
 describe('set up test', () => {
   it('assert response', () => {
     return asyncGetData(dataGet).then(data => {
+      expect(data).to.eql('somedata')
+    })
+  })
+  it('assert post', () => {
+    return asyncPostData(options).then(data => {
       expect(data).to.eql('somedata')
     })
   })
