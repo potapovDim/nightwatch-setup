0d837e23d7ec99eb6efd6708626833ecf88791ac
potapovDim <dmytrop@weblium.com>
2017-02-05 15:38:45 +0200

    refactor

--- apitests/test.spec.js
+++ apitests/test.spec.js
@@ -1,50 +1,11 @@
 const {expect} = require('chai')
-const util = require('./utils')
-const Rx = require('rxjs')
-const http = require('http')
-const querystring = require('querystring')
-
-
+const {asyncPostData, asyncGetData} = require('./utils')
 
 const dataGet = {
   host: 'localhost',
   port: '4422',
   path: '/'
 }
-
-
-
-const asyncGetData = data => new Promise((resolve, reject) => {
-  (getData = data => {
-    return http.get(data, response => {
-      let body = ''
-      response.on('data', response => {
-        body += response.toString('utf8')
-        if(body.statusCode ===404){
-          reject(body)
-        }
-      });
-      response.on('end', () => {
-        resolve(body)
-      })
-    })
-  })(data)
-})
-const asyncPostData = data => new Promise((resolve, reject) => {
-  (postData = data => {
-    return request.post(data,(error, response, body) => {
-      console.log(response.statusCode, body)
-      if(!error && response.statusCode === 200) {
-        resolve(body)
-      }
-      else {
-        reject(error)
-      }
-    })
-  })(data)
-})
-
-
 
 var postData = JSON.stringify({
   'name' : 'Hello World!'
@@ -61,22 +22,6 @@ var options = {
   }
 };
 
-const req = http.request(options, (res) => {
-  res.setEncoding('utf8')
-  res.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
-  })
-  res.on('end', () => {
-    console.log('No more data in response.');
-  })
-})
-
-req.on('error', (e) => {
-  console.log(`problem with request: ${e.message}`);
-});
-
-req.write(postData)
-req.end()
 
 
 describe('set up test', () => {
@@ -86,7 +31,7 @@ describe('set up test', () => {
     })
   })
   it('assert postdata', () => {
-    return asyncGetData(dataGet).then(data => {
+    return asyncPostData(postData, options).then(data => {
       expect(data).to.eql('somedata')
     })
   })
--- apitests/utils.js
+++ apitests/utils.js
@@ -1,13 +1,49 @@
+const http = require('http')
+const querystring = require('querystring')
+
 const toString = Object.prototype.toString
 
 const assertArray = arg => toString.call(arg) === '[object Array]'
-const assertString = arg => toString.call(arg) ==='[object String]'
-const assertNumber = arg => toString.call(arg) ==='[object Number]'
-const assertObject = arg => arg !== null && typeof arg ==='object'
+const assertString = arg => toString.call(arg) === '[object String]'
+const assertNumber = arg => toString.call(arg) === '[object Number]'
+const assertObject = arg => arg !== null && typeof arg === 'object'
+
+const asyncGetData = data => new Promise((resolve, reject) => http.get(data, response => {
+  let body = ''
+  response.on('data', response => {
+    body += response.toString('utf8')
+    if (body.statusCode === 404) {
+      reject(body)
+    }
+  });
+  response.on('end', () => {
+    resolve(body)
+  })
+}))
+
+const asyncPostData = (data, options) => new Promise((resolve, reject) => {
+  const req = http.request(options, (res) => {
+    res.setEncoding('utf8')
+    res.on('data', (chunk) => {
+      console.log(`BODY!!!!!!!!!!!!!!!!!!!!!!!!!!!!: ${chunk}`);
+    })
+    res.on('end', () => {
+      console.log('No more data in response.');
+    })
+  })
+  req.write(data)
+  req.end()
+  req.on('error', (e) => {
+    console.log(`problem with request: ${e.message}`);
+  });
+  resolve(true)
+})
 
 module.exports = {
   assertNumber,
   assertArray,
   assertObject,
-  assertString
+  assertString,
+  asyncGetData,
+  asyncPostData
 }
