45e16a1dd63b900d3fbc3dfd8446aa6154436c7a
IrynaY <yrchuk.iryna@gmail.com>
2017-02-23 12:27:43 +0200

    1st test

--- e2e/pages/infoPage.js
+++ e2e/pages/infoPage.js
@@ -1,18 +1,18 @@
 const disableCSS = require('../commands/disableCSS'); //require custom command 
 
-const initialPage = {
+const infoPage = {
   disableTransition (){
     return this.disableCSS(1) //nightwatch bug , run  custom command disable transition 
   },
-  waitApp(){
-    return this.waitForElementVisible('@app') //wait for render app
+  isBtnPresent(){
+    return this.btnReg();// waitForElementVisible('@app') //wait for render app
   }
 
 }
 module.exports = {
     url:"http://localhost:8080", //base url to your website
     elements: {
-      app: "#app" // css selector 
+      btnReg: ".create-user-button:first-child" // css selector 
     },
-    commands: [initialPage] //commands for this PO
+    commands: [infoPage] //commands for this PO
 }
--- e2e/tests/initialPage.js
+++ e2e/tests/initialPage.js
@@ -1,4 +1,6 @@
- module.exports =  {
+//var InfoPage = require('./classes/InfoPage');
+
+module.exports =  {
      'execute disable transition': (client) => {
          const initialPage = client.page.initialPage(); //initialize page
          initialPage.navigate() //go to url declare in page
@@ -6,5 +8,12 @@
          initialPage.waitApp() // wait render app
          initialPage.disableTransition() // execute script for disable css effects
          client.end() // end test
-     }
+     },
+
+
+    /* 'should have elements on Information  page' : (browser) => {
+         const infoPage = browser.page.infoPage();
+         browser.assert.elementPresent(infoPage.isBtnPresent())
+
+    }, */
  }
