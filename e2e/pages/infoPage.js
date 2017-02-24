c7ee76b0c91f0b2d505736f9acaaac5a75ca6891
IrynaY <yrchuk.iryna@gmail.com>
2017-02-24 09:23:32 +0200

    2nd

--- e2e/pages/infoPage.js
+++ e2e/pages/infoPage.js
@@ -1,18 +1,30 @@
 const disableCSS = require('../commands/disableCSS'); //require custom command 
 
 const infoPage = {
-  disableTransition (){
-    return this.disableCSS(1) //nightwatch bug , run  custom command disable transition 
+  isPresentBtnReg(){
+    return this.waitForElementPresent('@btnReg')
   },
-  isBtnPresent(){
-    return this.btnReg();// waitForElementVisible('@app') //wait for render app
+  isPresentBtnLogin(){
+    return this.waitForElementPresent('@btnLogin')
+  },
+  isPresentTimer(){
+    return this.waitForElementPresent('@timer')
+  },
+  isPresentInfoBlock(){
+    return this.waitForElementPresent('@informationBlock')
+  },
+  goToLogin(){
+    return this.click('@btnLogin')
   }
 
 }
 module.exports = {
     url:"http://localhost:8080", //base url to your website
     elements: {
-      btnReg: ".create-user-button:first-child" // css selector 
+      btnReg: ".create-user-button:first-child",
+      btnLogin: ".create-user-button:last-child",
+      timer: ".clock-time-show",
+      informationBlock : ".information"
     },
     commands: [infoPage] //commands for this PO
 }
---
+++ e2e/pages/loginPage.js
@@ -0,0 +1,31 @@
+const disableCSS = require('../commands/disableCSS'); //require custom command 
+
+const loginPage = {
+  isPresentLogin(){
+    return this.waitForElementPresent('@login')
+  },
+  isPresentPass(){
+    return this.waitForElementPresent('@pass')
+  },
+  isPresentBtnLogin(){
+    return this.waitForElementPresent('@btn')
+  },
+
+  authorization(login, pass){
+      this.setValue('@login', login)
+      this.setValue('@pass', pass)
+      this.click('@btn')
+  }
+ 
+}
+
+module.exports = {
+    url:"http://localhost:8080", //base url to your website
+    elements: {
+      login: ".user-name",
+      pass: ".user-password",
+      btn: ".btn" ,
+      app: "#app"
+    },
+    commands: [loginPage] //commands for this PO
+}
